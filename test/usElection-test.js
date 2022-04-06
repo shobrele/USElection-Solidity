e5737fbc8e0202e59b6afc8704af438b837ddccb
Dimitar Dzhalev <dimitar.dzhalev@gmail.com>
2022-04-06 15:38:48 +0300

    completed test coverage

---
+++ test/usElection-test.js
@@ -0,0 +1,66 @@
+const { expect } = require("chai");
+const {ethers} = require("hardhat");
+
+describe("USElection", function () {   
+    let usElectionFactory;
+    let usElection;
+    before(async () => {
+        usElectionFactory = await ethers.getContractFactory("USElection");
+        usElection = await usElectionFactory.deploy();
+        await usElection.deployed();
+    });
+    it("Should return the current leader before submit any election results", async function () {
+        expect(await usElection.currentLeader()).to.equal(0); // NOBODY
+    });
+
+    it("Should return the election status", async function () {
+        expect(await usElection.electionEnded()).to.equal(false); // Not Ended
+    });
+
+    it("Should submit state results and get current leader", async function () {
+        const stateResults = ["California",1000,900,32];
+        const submitStateResultsTx = await usElection.submitStateResult(stateResults);
+        await submitStateResultsTx.wait();
+        expect(await usElection.currentLeader()).to.equal(1); // BIDEN
+    });
+
+    it("Should throw when try to submit already submitted state results", async function () {
+        const stateResults = ["California",1000,900,32];
+        expect(usElection.submitStateResult(stateResults)).to.be.revertedWith('This state result was already submitted!');
+    });
+
+    it("Should submit state results and get current leader", async function () {
+        const stateResults = ["Ohaio",800,1200,33];
+        const submitStateResultsTx = await usElection.submitStateResult(stateResults);
+        await submitStateResultsTx.wait();
+        expect(await usElection.currentLeader()).to.equal(2); // TRUMP
+    });
+
+    it("Should throw when try to submit with equal state results", async function () {
+        const stateResults = ["Texas",1200,1200,33];
+        expect(usElection.submitStateResult(stateResults)).to.be.revertedWith('There cannot be a tie');
+    });
+
+    it("Should throw when try to submit with < 1 seat number", async function () {
+        const stateResults = ["Dallas",1200,200,0];
+        expect(usElection.submitStateResult(stateResults)).to.be.revertedWith('tates must have at least 1 seat');
+    });
+
+    it("Should throw on trying to end election with not the owner", async function () {
+        const [owner, addr1] = await ethers.getSigners();
+        expect(usElection.connect(addr1).endElection()).to.be.revertedWith('Ownable: caller is not the owner');
+        expect(await usElection.electionEnded()).to.equal(false); // Not Ended
+    });
+
+    it("Should end the elections, get the leader and election status", async function () {
+        const endElectionTx = await usElection.endElection();
+        await endElectionTx.wait();
+        expect(await usElection.currentLeader()).to.equal(2); // TRUMP
+        expect(await usElection.electionEnded()).to.equal(true); // Ended
+    });
+
+    it("Should throw when try to end election when they previously got ended already", async function () {
+        expect(usElection.endElection()).to.be.revertedWith('The election has ended already');
+    });
+
+});
